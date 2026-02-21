f580c394157496de6f68f2dc17312f7d32d61ba5
Agam Darmawan <madridlover085@gmail.com>
2026-02-21 17:40:19 +0700

    gg

--- Jobsheet2/CourseMain03.java
+++ Jobsheet2/CourseMain03.java
@@ -4,9 +4,9 @@ public class CourseMain03 {
     public static void main(String[] args) {
         // Object 1: default constructor
         Course03 c1 = new Course03();
-        c1.kodeCourse = "ASD101";
+        c1.kodeCourse = "SIB252004";
         c1.name = "Algoritma dan Struktur Data";
-        c1.Sks = 3;
+        c1.Sks = 2;
         c1.numberOfHours = 4;
 
         c1.DisplayInformation();
@@ -16,7 +16,7 @@ public class CourseMain03 {
         System.out.println();
 
         // Object 2: parameterized constructor
-        Course03 c2 = new Course03("BD201", "Basis Data", 3, 4);
+        Course03 c2 = new Course03("SIB252006", "Basis Data", 2, 4);
         c2.DisplayInformation();
         c2.reduceHours(10); // contoh gagal
     }
--- Jobsheet2/DosenMain03.java
+++ Jobsheet2/DosenMain03.java
@@ -4,20 +4,20 @@ public class DosenMain03 {
     public static void main(String[] args) {
         // def constructor
         Dosen03 d1 = new Dosen03();
-        d1.idLecturer = "DSN001";
-        d1.name = "Budi Santoso";
+        d1.idLecturer = "VAL";
+        d1.name = "Vivin Ayu Lestari, S.Pd., M.Kom.";
         d1.activeStatus = true;
         d1.yearOfEntry = 2018;
-        d1.expertiseCompetency = "Pemrograman Java";
+        d1.expertiseCompetency = "Dasar Pemrograman";
 
         d1.displayInformation();
         d1.setActiveStatus(false);
         System.out.println("Masa kerja: " + d1.calculateTimeWork(2026) + " tahun");
-        d1.changeSkill("Struktur Data");
+        d1.changeSkill("Algoritma dan Struktur Data");
         System.out.println();
 
         // parameterized constr
-        Dosen03 d2 = new Dosen03("DSN002", "Siti Aminah", true, 2020, "Basis Data");
+        Dosen03 d2 = new Dosen03("HP", "Hendra Pradibta, S.E., M.Sc.", true, 2020, "Analisis Proses Bisnis");
         d2.displayInformation();
         System.out.println("Masa kerja: " + d2.calculateTimeWork(2026) + " tahun");
     }
